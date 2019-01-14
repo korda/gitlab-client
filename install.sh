f4c45246cb21e57eb03abee039e04dd559e34559
Adrian Adamski <maidy.adr@gmail.com>
2019-01-14 17:25:52 +0100

    dodanie skryptu instalacyjnego

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+APP_FILE="gitlab-client.pyz"
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TARGET_DIR=$(mktemp -d -t installXXXXXXXXXXX)
+
+cd $SOURCE_DIR
+cp -r $(ls | grep -vE "^venv$") $TARGET_DIR
+
+rm -f $APP_FILE
+
+/usr/bin/env python3 -m zipapp -o $APP_FILE -p '/usr/bin/env python3' $TARGET_DIR
