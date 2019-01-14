bb658ac4100b422c154649acb0474ba3c9904c29
Adrian Adamski <maidy.adr@gmail.com>
2019-01-14 22:04:08 +0100

    dalsze porządki

--- __main__.py
+++ __main__.py
@@ -1,9 +1,7 @@
 import argparse
-from user_config import get_gitlab_api_client
-from user_config import get_project_dir_location
+
+from open_project import open_project_action
 from user_config import config_location
-from open_project import open_project
-from pathlib import Path
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Gitlab client.')
@@ -15,25 +13,9 @@ if __name__ == '__main__':
     parser.add_argument('args', nargs=argparse.REMAINDER)
 
     main_args = parser.parse_args()
-    gitlab_instance = main_args.gitlab_instance
 
     if main_args.action == 'open':
-        open_project_parser = argparse.ArgumentParser(description='Open project action',
-                                                      prog=f'{parser.prog} gitlab_instance open')
-        open_project_parser.add_argument('--save-dir-to', dest='saveDirTo',
-                                         help='dir path with checked out project will be stored in order to use in '
-                                              'bash function to this location')
-
-        args = open_project_parser.parse_args(main_args.args)
-
-        gitlab_api_client = get_gitlab_api_client(gitlab_instance)
-        project_dir = get_project_dir_location()
-        checkout_dir = open_project(gitlab_instance, gitlab_api_client, project_dir)
-
-        if args.saveDirTo:
-            Path(args.saveDirTo).write_text(checkout_dir + "\n")
+        open_project_action(main_args, parser.prog)
     else:
         print(f"Unsupported action {main_args.action}")
         quit(1)
-
-    #print(select_project("unity"))
--- open_project.py
+++ open_project.py
@@ -1,12 +1,35 @@
+import argparse
+from os.path import isdir
 from pathlib import Path
 from subprocess import check_call
-from os.path import isdir
 
 from curses_select import select_option
 from gitlab_api_client import GitlabApi
+from user_config import get_gitlab_api_client
+from user_config import get_project_dir_location
 
 
-def open_project(gitlab_instance: str, gitlab_api_client: GitlabApi, project_dir: str):
+def open_project_action(main_args, progname: str):
+    gitlab_instance = main_args.gitlab_instance
+
+    open_project_parser = argparse.ArgumentParser(description='Open project action',
+                                                  prog=f'{progname} gitlab_instance open')
+    open_project_parser.add_argument('--save-dir-to', dest='saveDirTo',
+                                     help='dir path with checked out project will be stored in order to use in '
+                                          'bash function to this location')
+
+    args = open_project_parser.parse_args(main_args.args)
+
+    gitlab_api_client = get_gitlab_api_client(gitlab_instance)
+    project_dir = get_project_dir_location()
+
+    checkout_dir = __open_project(gitlab_instance, gitlab_api_client, project_dir)
+
+    if args.saveDirTo:
+        Path(args.saveDirTo).write_text(checkout_dir + "\n")
+
+
+def __open_project(gitlab_instance: str, gitlab_api_client: GitlabApi, project_dir: str):
     projects = [project["path_with_namespace"] for project in gitlab_api_client.all_projects()]
 
     projects.sort()
